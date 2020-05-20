270cc1b1e5764d2a23628bd2cba4a6fd27401af9
Léo Delplanque <delplanqueleomail@gmail.com>
2020-05-20 13:39:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="node_modules/modern-normalize/modern-normalize.css">
+    <link rel="stylesheet" href="assets/style/master.css">
+    <title>Ekko - Web Agency</title>
+</head>
+<body>
+    
+
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="assets/script/script.js"></script>
+</body>
+</html>

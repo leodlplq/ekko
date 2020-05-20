b10cbec10908b1bf5acad6ce9e8c11ad8f7c1440
Léo Delplanque <delplanqueleomail@gmail.com>
2020-05-20 15:25:00 +0200

    partie droite du site

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,5 +3,12 @@
 
 
 $(document).ready(()=>{
-   
+    
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 50) {
+          $(".right").addClass("scrolled");
+        } else {
+          $(".right").removeClass("scrolled");
+        }
+      });
 });
--- index.php
+++ index.php
@@ -6,12 +6,38 @@
 
     <link rel="stylesheet" href="node_modules/modern-normalize/modern-normalize.css">
     <link rel="stylesheet" href="assets/style/master.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;800&display=swap" rel="stylesheet">
     <title>Ekko - Web Agency</title>
 </head>
 <body>
-    <div class="left"></div>
+    <div class="left">
+        <div class="title">Nous sommes</div>
+
+
+    </div>
     <div class="right">
-        <div class="menu"></div>
+        <div class="menu">
+            <a href="#" class="link_menu">Accueil</a>
+            <a href="#" class="link_menu">Nos projets</a>
+            <a href="#" class="link_menu">L'équipe</a>
+            <a href="#" class="link_menu">Contact</a>
+        </div>
+
+        <div class="runes">
+            <img src="assets/images/rune.png" alt="rune">
+        </div>
+
+        <div class="infos">
+            <div class="logo">
+                <img src="assets/images/logo.svg" alt="Logo de Ekko">
+            </div>
+            <div class="baseline">Avec nous faites rayonner vos projets.</div>
+            <a href="#" class="btn">
+                Un projet ?
+            </a>
+        </div>
+
+
         
     </div>
 

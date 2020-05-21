e48338f6dc1d9b1235bcede8c4d1e1db6613d586
Léo Delplanque <delplanqueleomail@gmail.com>
2020-05-21 15:29:08 +0200

    ajout du caroussel des projets

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,13 +3,61 @@
 
 
 $(document).ready(()=>{
+    function createGlide(){
+      var select = document.querySelector('.glide')
 
+      var glide = new Glide('.glide', {
+        type: select.value,
+        focusAt: 'center',
+        perView: 1
+      })
+  
+      select.addEventListener('change', function (event) {
+        glide.update({
+          type: event.target.value
+        })
+      })
+  
+      glide.mount()
+    }
+    
     $(window).scroll(function() {
         
+
+        console.log($(document).scrollTop())
         if ($(document).scrollTop() > 150) {
           $(".right").addClass("scrolled");
         } else {
           $(".right").removeClass("scrolled");
-        }
+        } 
+
+        if ($(document).scrollTop() > 1000) {
+            $(".infos").addClass("animate");
+          } else {
+            $(".infos").removeClass("animate");
+          } 
+
+        if ($(document).scrollTop() > 1150) {
+            
+          setTimeout(()=>{
+            createGlide()
+            $('.caroussel').show  ();
+            $('.caroussel').addClass("scrolledPlusC");
+          
+          }, 1000);
+
+            $(".right").addClass("scrolledPlus");
+          } else {
+            $(".right").removeClass("scrolledPlus");
+            $('.caroussel').removeClass("scrolledPlusC");
+            setTimeout(()=>{
+             
+              $('.caroussel').hide();
+            
+            }, 1000);
+          } 
+
+
+
       });
 });
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="node_modules/modern-normalize/modern-normalize.css">
+    <link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.core.min.css">
     <link rel="stylesheet" href="assets/style/master.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;800&display=swap" rel="stylesheet">
     <title>Ekko - Web Agency</title>
@@ -13,7 +14,7 @@
     <div class="left">
         <div class="title">Nous sommes</div>
 
-
+        <img class="mockup" src="assets/images/mockup.png" alt="Mock up">
 
     </div>
     <div class="right">
@@ -37,14 +38,82 @@
                 Un projet ?
             </a>
         </div>
+        <div class="caroussel">
+            <div class="glide">
+                <div class="glide__track" data-glide-el="track">
+                    <ul class="glide__slides">
+                        <li class="glide__slide">
+                        <img src="assets/images/mockup.png" alt="mockup">
+                            <div class="texte_projet">
+                                <div class="title_projet">Enl'air</div>
+                                <div class="desc_projet">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus scelerisque odio aliquet eleifend eget. </div>
+                                <div class="avis_projet">
+                                    <span class="texte_avis_projet">“ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus scelerisque odio aliquet eleifend eget. ”</span>
+                                    <span class="nom_avis_projet">Mme Laure.M, Agence de Voyage ENl’AIR</span>
+                                </div>
+                                <a href="#" class="lien_projet">www.enlair.fr</a>
+                            </div>
+                        </li>
+                        <li class="glide__slide">
+                        <img src="assets/images/mockup.png" alt="mockup">
+                            <div class="texte_projet">
+                                <div class="title_projet">Enl'air</div>
+                                <div class="desc_projet">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus scelerisque odio aliquet eleifend eget. </div>
+                                <div class="avis_projet">
+                                    <span class="texte_avis_projet">“ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus scelerisque odio aliquet eleifend eget. ”</span>
+                                    <span class="nom_avis_projet">Mme Laure.M, Agence de Voyage ENl’AIR</span>
+                                </div>
+                                <a href="#" class="lien_projet">www.enlair.fr</a>
+                            </div>
+                         </li>
+                         <li class="glide__slide">
+                         <img src="assets/images/mockup.png" alt="mockup">
+                            <div class="texte_projet">
+                                <div class="title_projet">Enl'air</div>
+                                <div class="desc_projet">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus scelerisque odio aliquet eleifend eget. </div>
+                                <div class="avis_projet">
+                                    <span class="texte_avis_projet">“ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus scelerisque odio aliquet eleifend eget. ”</span>
+                                    <span class="nom_avis_projet">Mme Laure.M, Agence de Voyage ENl’AIR</span>
+                                </div>
+                                <a href="#" class="lien_projet">www.enlair.fr</a>
+                            </div>
+                         </li>
+                         <li class="glide__slide">
+                         <img src="assets/images/mockup.png" alt="mockup">
+                            <div class="texte_projet">
+                                <div class="title_projet">Enl'air</div>
+                                <div class="desc_projet">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus scelerisque odio aliquet eleifend eget. </div>
+                                <div class="avis_projet">
+                                    <span class="texte_avis_projet">“ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus scelerisque odio aliquet eleifend eget. ”</span>
+                                    <span class="nom_avis_projet">Mme Laure.M, Agence de Voyage ENl’AIR</span>
+                                </div>
+                                <a href="#" class="lien_projet">www.enlair.fr</a>
+                            </div>
+                         </li>
+                    </ul>
+                </div>
 
+                <div class="glide__arrows" data-glide-el="controls">
+                    <button class="glide__arrow glide__arrow--left" data-glide-dir="<"><img src="assets/images/leftarrow.png" alt="fleche gauche"></button>
+                    <button class="glide__arrow glide__arrow--right" data-glide-dir=">"><img src="assets/images/rightarrow.png" alt="fleche droite"></button>
+                </div>
 
+                <div class="glide__bullets" data-glide-el="controls[nav]">
+                    <button class="glide__bullet" data-glide-dir="=0"></button>
+                    <button class="glide__bullet" data-glide-dir="=1"></button>
+                    <button class="glide__bullet" data-glide-dir="=2"></button>
+                    <button class="glide__bullet" data-glide-dir="=3"></button>
+                </div>
+            </div>
+        
+        </div>
+        
         
     </div>
 
 
     
-
+<script src="node_modules/@glidejs/glide/dist/glide.min.js"></script>
 <script src="node_modules/jquery/dist/jquery.min.js"></script>
 <script src="assets/script/script.js"></script>
 </body>
